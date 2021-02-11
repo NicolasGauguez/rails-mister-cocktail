06541725e4155d300411cd8615124c7df88a5eaa
Nicolas Gauguez <76942939+NicolasGauguez@users.noreply.github.com>
2021-02-11 17:15:30 +0100

    Mister cocktail

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,10 +1,26 @@
 class DosesController < ApplicationController
-  def create
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
   end
 
-  def new
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    @dose.save
+    redirect_to cocktails_path
   end
 
   def destroy
+    @dose_params = Dose_params.find(params[:id])
+    @dose_params.destroy
+    redirect_to dose_paramss_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  get 'doses/create'
-  get 'doses/new'
-  get 'doses/destroy'
-  get 'cocktails/index'
-  get 'cocktails/show'
-  get 'cocktails/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [ :index, :show, :create, :new ] do
-    resources :doses, only: [ :new, :create ]
+  resources :cocktails, only: [:index, :show, :create, :new] do
+    resources :doses, only: [:new, :create]
   end
-  resources :doses, only: [ :destroy]
+  resources :doses, only: [:destroy]
 end
